6582b9e5c05daa2b695718b4aba4c4d79cc9d7da
Kamil Plich <kamilplich2@gmail.com>
2018-04-08 17:50:17 +0200

    docs a few extra fields

--- src/sim/Simulator.java
+++ src/sim/Simulator.java
@@ -2,32 +2,79 @@ package sim;
 
 import java.util.*;
 
+/**
+ * An abstract class providing a base for each paging algorithm.
+ */
 public abstract class Simulator {
-	Random generator = new Random();
+	/**
+	 * Random number generator, used mainly to generate requests.
+	 */
+	Random rng = new Random();
 
-	LinkedList<Integer> requestQueue;
-	int[] physicalMemory;
-	int virtualMemory;
+	/**
+	 * Queue containing all the page requests.
+	 */
+	protected LinkedList<Integer> requestQueue;
 
-	public Simulator(int simulationSize, int pages, int frames) {
+	/**
+	 * This table implements the physical memory - each cell of a table represents a frame in memory.
+	 * Each frame contains the number of a page it's holding in its memory. If a frame isn't used, it contains a -1.
+	 */
+	protected int[] physicalMemory;
+
+	/**
+	 * Number of frames used. Most of the time equal to the size of physical memory, smaller only at the beginning of
+	 * the simulation
+	 */
+	protected int framesUsed;
+
+	/**
+	 * I suppose this variable is meant to indicate the size of virtual memory -
+	 * - the number of pages that can be allocated.
+	 */
+	protected int virtualMemory;
+
+	/**
+	 * This variable holds a number of times a page wasn't found in physical memory and had to be loaded from VM.
+	 */
+	protected int frameMissCount;
+
+	/**
+	 * Constructor, lol.
+	 * @param pages size of virtual memory - number of pages possible to allocate.
+	 * @param frames size of physical memory - number of frames to hold pages.
+	 */
+	public Simulator(int pages, int frames) {
 		virtualMemory = pages;
 		physicalMemory = new int[frames];
-		generateRequests(simulationSize);
+		Arrays.fill(physicalMemory, -1); //no pages allocated yet
 	}
 
+	/**
+	 * Generates a queue of requests to memory
+	 * @param size number of requests to be generated
+	 */
 	private void generateRequests(int size) {
 		requestQueue = new LinkedList<>();
 
 		for(int i = 0; i<size; ++i) {
-			requestQueue.add(generator.nextInt(virtualMemory) + 1);
+			requestQueue.add(rng.nextInt(virtualMemory) + 1);
 		}
 	}
 
-	public void run() {
+	/**
+	 * This method runs the simulation - on each loop run attempts to access a given page.
+	 * @param simulationSize numer of pages to be accessed in a simulation
+	 */
+	public void run(int simulationSize) {
+		generateRequests(simulationSize);
+
 		while(!requestQueue.isEmpty()) {
-			allocatePage();
+			accessPage();
 		}
 	}
 
-	protected abstract void allocatePage();
+	private void accessPage() {
+
+	}
 }
