ece20d82461143865ffb9c9c5291a5de727a7af9
Kamil Plich <kamilplich2@gmail.com>
2018-04-08 18:23:02 +0200

    created Page class

---
+++ src/sim/Page.java
@@ -0,0 +1,37 @@
+package sim;
+
+/**
+ * Simple wrapper class created to make creating a sortable page table possible.
+ */
+public class Page {
+	/**
+	 * Number of a page.
+	 */
+	private int pageNumber;
+
+	/**
+	 * Index of a frame a page has been given (or -1 if page isn't loaded in to memory).
+	 */
+	private int frameGiven;
+
+	public Page(int pageNumber, int frameGiven) {
+		this.pageNumber = pageNumber;
+		this.frameGiven = frameGiven;
+	}
+
+	public int getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public int getFrameGiven() {
+		return frameGiven;
+	}
+
+	public void setFrameGiven(int frameGiven) {
+		this.frameGiven = frameGiven;
+	}
+}
